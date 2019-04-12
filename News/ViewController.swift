292ea7d16482a6615949568d3ffc80080e355660
kawochan <warren.kw.chan@alumni.ubc.ca>
2019-04-11 23:51:45 -0700

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Warren Chan on 2019-04-11.
+//  Copyright © 2019 Warren Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
